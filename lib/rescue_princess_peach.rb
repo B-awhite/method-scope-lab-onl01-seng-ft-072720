88c165de37206897f5707685a0ea42ac04371f8f
Bria White <bria.awhite@gmail.com>
2020-07-30 03:23:00 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,9 +1,9 @@
-status = "rescued"
+
 
 def rescue_princess_peach
   puts "Hooray! Mario has rescued Princess Peach."
 end
-
+status = "rescued"
 ## Comment the below back in to see method scope in action!
 
 # rescue_princess_peach
